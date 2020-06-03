33f383e313095e4f98ee751357a15dd929292b19
Buchaillat Rémi <buchaillat.remi@hotmail.com>
2020-06-03 16:54:18 +0200

    [EDIT] - Design of login component

--- src/components/form/login-form.js
+++ src/components/form/login-form.js
@@ -1,26 +1,36 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { Link } from "react-router-dom";
+import {
+  TextField,
+  CircularProgress,
+  Grid,
+  Button,
+  Avatar,
+  Typography,
+} from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
+import { Lock as LockIcon } from "@material-ui/icons";
 
 import { useAsync } from "../../hooks/async-hook";
 import { useAuth } from "../../context/auth-context";
-import { useFormik } from "formik";
-
-import * as yup from "yup";
-
-import { Link } from "react-router-dom";
-import FormLabel from "@material-ui/core/FormLabel";
-import TextField from "@material-ui/core/TextField";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
-import Alert from "@material-ui/lab/Alert";
+import { loginStyle } from "../../styles/components/login-style";
 
 function LoginForm() {
   const { isLoading, isError, error, run } = useAsync();
   const { login } = useAuth();
+  const classes = loginStyle();
 
   const validationSchema = yup.object({
-    email: yup.string().required().email(),
-    password: yup.string().required().min(8),
+    email: yup
+      .string()
+      .required("Email is required")
+      .email("Email must be a valid email"),
+    password: yup
+      .string()
+      .required("Password is required")
+      .min(8, "Password must be at least 8 characters"),
   });
 
   const loginForm = useFormik({
@@ -44,48 +54,61 @@ function LoginForm() {
   });
 
   return (
-    <form noValidate autoComplete="off" onSubmit={loginForm.handleSubmit}>
-      <FormLabel>Login</FormLabel>
-      {isError ? <Alert severity="error">{error.message}</Alert> : null}
-
-      <TextField
-        error={!!loginForm.errors.email}
-        id="email"
-        label="Email"
-        type="email"
-        defaultValue={loginForm.values.email}
-        helperText={loginForm.errors.email}
-        onChange={loginForm.handleChange}
-        variant="outlined"
-        margin="normal"
-      />
-      <TextField
-        error={!!loginForm.errors.password}
-        id="password"
-        label="Password"
-        type="password"
-        onChange={loginForm.handleChange}
-        defaultValue={loginForm.values.password}
-        helperText={loginForm.errors.password}
-        variant="outlined"
-        margin="normal"
-      />
-      <Button type="submit" fullWidth variant="contained" color="primary">
-        Login {isLoading ? <CircularProgress /> : null}
-      </Button>
-      <Grid container>
-        <Grid item>
-          <span>
-            No account ? <Link to="/register">Register</Link>
-          </span>
+    <section className={classes.root}>
+      <Avatar className={classes.avatar}>
+        <LockIcon />
+      </Avatar>
+      <Typography className={classes.typography} component="h1" variant="h5">
+        Login
+      </Typography>
+      <form
+        className={classes.form}
+        noValidate
+        autoComplete="off"
+        onSubmit={loginForm.handleSubmit}
+      >
+        {isError ? <Alert severity="error">{error.message}</Alert> : null}
+        <TextField
+          className={classes.field}
+          error={!!loginForm.errors.email}
+          id="email"
+          label="Email"
+          type="email"
+          defaultValue={loginForm.values.email}
+          helperText={loginForm.errors.email}
+          onChange={loginForm.handleChange}
+          variant="outlined"
+          margin="normal"
+        />
+        <TextField
+          className={classes.field}
+          error={!!loginForm.errors.password}
+          id="password"
+          label="Password"
+          type="password"
+          onChange={loginForm.handleChange}
+          defaultValue={loginForm.values.password}
+          helperText={loginForm.errors.password}
+          variant="outlined"
+          margin="normal"
+        />
+        <Button type="submit" fullWidth variant="contained" color="primary">
+          Login {isLoading ? <CircularProgress /> : null}
+        </Button>
+        <Grid container>
+          <Grid item>
+            <Typography variant="body2" className={classes.forgot}>
+              <Link to="/register">Vous n'avez pas de compte ?</Link>
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="body2" className={classes.register}>
+              <Link to="/reset-password">Mot de passe oublié ?</Link>
+            </Typography>
+          </Grid>
         </Grid>
-        <Grid item>
-          <span>
-            Forgot Password ? <Link to="/reset-password">Reset</Link>
-          </span>
-        </Grid>
-      </Grid>
-    </form>
+      </form>
+    </section>
   );
 }
 
--- src/unauthenticated-app.js
+++ src/unauthenticated-app.js
@@ -1,21 +1,11 @@
 import React from "react";
 
-import AppLogo from "./components/app-logo";
-import Grid from "@material-ui/core/Grid";
 import UnauthenticatedRoutes from "./routes/unauthenticated-routes";
 
 function UnauthenticatedApp() {
   return (
     <section>
-      <Grid container>
-        <Grid item xs={6}>
-          <AppLogo />
-          <p>Welcome, ready ti share your best songs with your friend ?</p>
-        </Grid>
-        <Grid item xs={6}>
-          <UnauthenticatedRoutes />
-        </Grid>
-      </Grid>
+      <UnauthenticatedRoutes />
     </section>
   );
 }

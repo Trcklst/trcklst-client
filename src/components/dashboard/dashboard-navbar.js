5ccdd4984f069c5c150b2299f212afc40773a63c
Buchaillat Rémi <buchaillat.remi@hotmail.com>
2020-06-14 16:59:22 +0200

    clean

--- src/components/dashboard/dashboard-navbar.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function DashboardNavbar() {
-  return <nav>nav</nav>;
-}
-
-export default DashboardNavbar;
--- src/components/user/user-avatar.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function UserAvatar({ src }) {
-  return <img src={src} alt="user-avatar" />;
-}
-
-export default UserAvatar;

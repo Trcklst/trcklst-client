e0494732ed1db92c87ece71afdc528f1fde9319a
Buchaillat Rémi <buchaillat.remi@hotmail.com>
2020-07-11 13:27:56 +0200

    [EDIT] - rework on join/leave party | fix socket to updated

--- src/components/party/join/Party.js
+++ src/components/party/join/Party.js
@@ -5,26 +5,13 @@ import { useHistory } from "react-router-dom";
 import { useStyles } from "./useStyles";
 import { initialValues, PartyForm } from "./PartyForm";
 import { partySchema } from "./partySchema";
-import { Party } from "../../../services/party";
-import { MYPARTIES } from "../../../helpers/route-constant";
-import { success, fail } from "../../common/Toast";
 
 export const PartyJoin = () => {
   const classes = useStyles();
   const { push } = useHistory();
 
   const handleSubmit = async ({ idParty }) => {
-    const data = await Party.join(idParty);
-    const jsonData = await data.json();
-
-    if (data.status !== 200) {
-      fail("Une erreur s'est produite en essayant de rejoindre une party.");
-      throw jsonData;
-    }
-
-    success("Vous avez rejoint la party.");
-
-    return push(MYPARTIES);
+    return push(`/party/${idParty}`);
   };
 
   return (

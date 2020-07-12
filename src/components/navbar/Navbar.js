357469e7a79233300c0052a50966800f1c48e1bf
Lavan350 <lavan.prepanantha@uni-medias.com>
2020-07-12 18:22:19 +0200

    [UPDATE] landingpage

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -15,7 +15,6 @@ import { SessionContext } from "../../context/session";
 import { Box, Container } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import { LOGIN, REGISTER, HOME } from "../../helpers/route-constant";
-import { logoWhite2 } from "../../images/logo_white2.png";
 
 export const Navbar = () => {
   const [drawer, setDrawer] = useState({ open: false });
@@ -53,7 +52,7 @@ export const Navbar = () => {
             )}
             <Typography variant="h6" className={classes.title}>
               <Link to={HOME} className={classes.link}>
-                <img src={logoWhite2} alt="logoWhite" />
+                Trcklst
               </Link>
             </Typography>
             {Object.keys(user).length !== 0 ? (

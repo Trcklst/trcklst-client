6b797114ae8c35bd5eb3cfcd75f6c25516b0de99
rbuchaillat <32840729+rbuchaillat@users.noreply.github.com>
2020-07-13 00:22:29 +0200

    Update routes-notconnected.js

--- src/routes/routes-notconnected.js
+++ src/routes/routes-notconnected.js
@@ -4,8 +4,8 @@ import { Route, Switch, Redirect } from "react-router-dom";
 import { Homepage } from "../components/homepage/Homepage";
 import { Login } from "../components/login/Login";
 import { Register } from "../components/register/Register";
-import { Forgotpassword } from "../components/forgotpassword/Forgotpassword";
-import { Newpassword } from "../components/forgotpassword/Newpassword";
+import { ForgotPassword } from "../components/forgotpassword/ForgotPassword";
+import { NewPassword } from "../components/forgotpassword/NewPassword";
 
 import { NotFound } from "../components/NotFound";
 import { NOTFOUND, HOME, LOGIN, REGISTER, FORGOTPASSWORD, NEWPASSWORD } from "../helpers/route-constant";
@@ -16,8 +16,8 @@ export const RoutesNotConnected = () => {
       <Route exact path={HOME} component={Homepage}></Route>
       <Route exact path={LOGIN} component={Login}></Route>
       <Route exact path={REGISTER} component={Register}></Route>
-      <Route exact path={FORGOTPASSWORD} component={Forgotpassword}></Route>
-      <Route exact path={NEWPASSWORD} component={Newpassword}></Route>
+      <Route exact path={FORGOTPASSWORD} component={ForgotPassword}></Route>
+      <Route exact path={NEWPASSWORD} component={NewPassword}></Route>
       <Route exact path={NOTFOUND} component={NotFound} />
       <Redirect to={NOTFOUND} />
     </Switch>

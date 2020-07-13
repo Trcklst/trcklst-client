1302748379d8a9280638e492eef69b26114d3096
Buchaillat Rémi <buchaillat.remi@hotmail.com>
2020-07-13 19:52:01 +0200

    [ADD] - unlike in music

--- src/components/party/show/playlist/Music.js
+++ src/components/party/show/playlist/Music.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { makeStyles, Grid, Box, IconButton, Badge } from "@material-ui/core";
-import FavoriteIcon from "@material-ui/icons/Favorite";
+import {
+  ThumbDown as ThumbDownIcon,
+  ThumbUp as ThumbUpIcon,
+} from "@material-ui/icons";
 
 import { Track } from "../../../../services/track";
 
@@ -18,11 +21,26 @@ const useStyles = makeStyles({
   },
 });
 
-export const Music = ({ id, icon, title, votesCount, idParty }) => {
+export const Music = ({
+  id,
+  icon,
+  title,
+  votesCount,
+  votes,
+  user,
+  idParty,
+}) => {
   const classes = useStyles();
 
-  const handleClick = async () => {
+  const handleClickLike = async () => {
     const data = await Track.vote(idParty, id);
+    const jsonData = await data.json();
+
+    if (data.status !== 200) throw jsonData;
+  };
+
+  const handleClickUnLike = async () => {
+    const data = await Track.unvote(idParty, id);
     const jsonData = await data.json();
 
     if (data.status !== 200) throw jsonData;
@@ -40,15 +58,27 @@ export const Music = ({ id, icon, title, votesCount, idParty }) => {
       </Grid>
       <Grid item xs={2}>
         <Badge color="secondary" badgeContent={votesCount}>
-          <IconButton
-            style={{ padding: 1 }}
-            aria-label="like"
-            onClick={() => {
-              handleClick();
-            }}
-          >
-            <FavoriteIcon style={{ color: "#3f51b5" }} />
-          </IconButton>
+          {votes.includes(user.userId) ? (
+            <IconButton
+              style={{ padding: 1 }}
+              aria-label="like"
+              onClick={() => {
+                handleClickUnLike();
+              }}
+            >
+              <ThumbDownIcon style={{ color: "#3f51b5" }} />
+            </IconButton>
+          ) : (
+            <IconButton
+              style={{ padding: 1 }}
+              aria-label="like"
+              onClick={() => {
+                handleClickLike();
+              }}
+            >
+              <ThumbUpIcon style={{ color: "#3f51b5" }} />
+            </IconButton>
+          )}
         </Badge>
       </Grid>
     </Grid>
